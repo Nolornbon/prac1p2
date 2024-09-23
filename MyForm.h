812aa51f55e3ceefed30412332d1eaf9d8239360
Misha Smishnyi <mishasmish@gmail.com>
2024-09-23 09:55:18 +0300

    Lab1Task1

--- MyForm.h
+++ MyForm.h
@@ -48,6 +48,17 @@ namespace prac1p2 {
 	private: System::Windows::Forms::PictureBox^ pictureBox2;
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::ComboBox^ comboBox2;
+
+
+	private: System::Windows::Forms::CheckedListBox^ checkedListBox1;
+	private: System::Windows::Forms::ListBox^ listBox1;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::LinkLabel^ linkLabel1;
+	private: System::Windows::Forms::ComboBox^ comboBox3;
+	private: System::Windows::Forms::ComboBox^ comboBox4;
 
 
 
@@ -85,6 +96,15 @@ namespace prac1p2 {
 			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
+			this->checkedListBox1 = (gcnew System::Windows::Forms::CheckedListBox());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->linkLabel1 = (gcnew System::Windows::Forms::LinkLabel());
+			this->comboBox3 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox4 = (gcnew System::Windows::Forms::ComboBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
 			this->SuspendLayout();
@@ -202,14 +222,13 @@ namespace prac1p2 {
 			this->pictureBox2->TabIndex = 12;
 			this->pictureBox2->TabStop = false;
 			this->pictureBox2->Visible = false;
-			this->pictureBox2->Click += gcnew System::EventHandler(this, &MyForm::pictureBox2_Click);
 			// 
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 19.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
 			this->button1->ImageAlign = System::Drawing::ContentAlignment::TopCenter;
-			this->button1->Location = System::Drawing::Point(551, 262);
+			this->button1->Location = System::Drawing::Point(551, 238);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(122, 54);
 			this->button1->TabIndex = 13;
@@ -222,7 +241,7 @@ namespace prac1p2 {
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 19.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button2->Location = System::Drawing::Point(733, 262);
+			this->button2->Location = System::Drawing::Point(730, 238);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(118, 54);
 			this->button2->TabIndex = 14;
@@ -231,11 +250,115 @@ namespace prac1p2 {
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
+			// comboBox2
+			// 
+			this->comboBox2->FormattingEnabled = true;
+			this->comboBox2->Items->AddRange(gcnew cli::array< System::Object^  >(4) { L"1", L"2", L"3", L"4" });
+			this->comboBox2->Location = System::Drawing::Point(36, 311);
+			this->comboBox2->Name = L"comboBox2";
+			this->comboBox2->Size = System::Drawing::Size(155, 24);
+			this->comboBox2->TabIndex = 15;
+			this->comboBox2->Text = L"UniversityYear";
+			// 
+			// checkedListBox1
+			// 
+			this->checkedListBox1->FormattingEnabled = true;
+			this->checkedListBox1->Items->AddRange(gcnew cli::array< System::Object^  >(7) {
+				L"Programming", L"Linear algebra", L"Analytical geometry",
+					L"Math analysis", L"History", L"Mathematical statistics", L"Differential equations"
+			});
+			this->checkedListBox1->Location = System::Drawing::Point(39, 349);
+			this->checkedListBox1->Name = L"checkedListBox1";
+			this->checkedListBox1->Size = System::Drawing::Size(241, 106);
+			this->checkedListBox1->TabIndex = 19;
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->ItemHeight = 16;
+			this->listBox1->Location = System::Drawing::Point(310, 349);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->ScrollAlwaysVisible = true;
+			this->listBox1->Size = System::Drawing::Size(214, 100);
+			this->listBox1->TabIndex = 20;
+			this->listBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MyForm::listBox1_SelectedIndexChanged);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(204, 475);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(142, 33);
+			this->button3->TabIndex = 21;
+			this->button3->Text = L"I study";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(551, 319);
+			this->textBox4->Multiline = true;
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(296, 135);
+			this->textBox4->TabIndex = 22;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(627, 475);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(142, 33);
+			this->button4->TabIndex = 23;
+			this->button4->Text = L"About me";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
+			// linkLabel1
+			// 
+			this->linkLabel1->AutoSize = true;
+			this->linkLabel1->Location = System::Drawing::Point(12, 495);
+			this->linkLabel1->Name = L"linkLabel1";
+			this->linkLabel1->Size = System::Drawing::Size(41, 16);
+			this->linkLabel1->TabIndex = 24;
+			this->linkLabel1->TabStop = true;
+			this->linkLabel1->Text = L"Repo";
+			this->linkLabel1->LinkClicked += gcnew System::Windows::Forms::LinkLabelLinkClickedEventHandler(this, &MyForm::linkLabel1_LinkClicked);
+			// 
+			// comboBox3
+			// 
+			this->comboBox3->FormattingEnabled = true;
+			this->comboBox3->Items->AddRange(gcnew cli::array< System::Object^  >(3) { L"M", L"COM", L"COI" });
+			this->comboBox3->Location = System::Drawing::Point(197, 311);
+			this->comboBox3->Name = L"comboBox3";
+			this->comboBox3->Size = System::Drawing::Size(155, 24);
+			this->comboBox3->TabIndex = 25;
+			this->comboBox3->Text = L"Group";
+			// 
+			// comboBox4
+			// 
+			this->comboBox4->FormattingEnabled = true;
+			this->comboBox4->Items->AddRange(gcnew cli::array< System::Object^  >(3) {
+				L"111 Mathematics", L"014.04 Edu(Mathematics)",
+					L"014.09 Edu(Computer science)"
+			});
+			this->comboBox4->Location = System::Drawing::Point(358, 311);
+			this->comboBox4->Name = L"comboBox4";
+			this->comboBox4->Size = System::Drawing::Size(166, 24);
+			this->comboBox4->TabIndex = 26;
+			this->comboBox4->Text = L"Specialty";
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1055, 520);
+			this->Controls->Add(this->comboBox4);
+			this->Controls->Add(this->comboBox3);
+			this->Controls->Add(this->linkLabel1);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->checkedListBox1);
+			this->Controls->Add(this->comboBox2);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->pictureBox2);
@@ -274,7 +397,30 @@ private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e
 	pictureBox2->Visible = true;
 	pictureBox1->Visible = false;
 }
-private: System::Void pictureBox2_Click(System::Object^ sender, System::EventArgs^ e) {
+
+private: System::Void linkLabel1_LinkClicked(System::Object^ sender, System::Windows::Forms::LinkLabelLinkClickedEventArgs^ e) {
+	System::Diagnostics::Process::Start("https://github.com/Nolornbon?tab=overview&from=2022-12-01&to=2022-12-31");
+}
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	listBox1->Items->Clear();
+	for (int i = 0; i < checkedListBox1->CheckedItems->Count; i++) {
+		listBox1->Items->Add(checkedListBox1->CheckedItems[i]);
+	}
+
+}
+private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+	textBox4->Text = "Surname: " + textBox1->Text + "\r\n";
+	textBox4->Text += "Name: " + textBox2->Text + "\r\n";
+	textBox4->Text += "BirthDate: " + maskedTextBox2->Text + "\r\n";
+	textBox4->Text += "Address: " + textBox3->Text + "\r\n";
+	textBox4->Text += "Country: " + comboBox1->Text + "\r\n";
+	textBox4->Text += "UniversityYear: " + comboBox2->Text + "\r\n";
+	textBox4->Text += "Group: " + comboBox3->Text + "\r\n";
+	textBox4->Text += "Specialty: " + comboBox4->Text + "\r\n";
+}
+
+private: System::Void listBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+	listBox1->Items->Remove(listBox1->SelectedItem);
 }
 };
 }
